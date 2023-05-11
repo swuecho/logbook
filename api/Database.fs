a8546bc28b575e4f6172fd7a6167b875ceb203c3
Hao Wu <wuhaoecho@gmail.com>
2023-05-11 20:16:09 +0800

    update

--- api/Database.fs
+++ api/Database.fs
@@ -8,7 +8,7 @@ module Config =
     /// Custom operator for combining paths
     let postgresDSN =
         try
-            System.Environment.GetEnvironmentVariable("BESTQA_PG_DSN")
+            System.Environment.GetEnvironmentVariable("DATABASE_URL")
         with _ -> raise (System.Exception "check if BESTQA_FS_PORT is set")
     let connection () = postgresDSN |> Sql.connect
 
