2c262c4abbd3eaaa1f243321e489e8e7e7b5507a
Hao Wu <wuhaoecho@gmail.com>
2023-05-12 14:26:09 +0800

    sqlc generate

--- api/sql/schema.sql
+++ api/sql/schema.sql
@@ -1,15 +1,15 @@
 
 CREATE TABLE IF NOT EXISTS diary (
     id character varying(8) NOT NULL,
-    note text
-    last_updated timestamp with timezone DEFAULT NOW(),
+    note text DEFAULT '' NOT NULL,
+    last_updated timestamp with time zone DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS summary (
     id character varying(8) NOT NULL,
-    created_at timestamp with time zone DEFAULT NOW(),
-    last_updated timestamp with time zone DEFAULT NOW(),
-    content jsonb,
+    created_at timestamp with time zone DEFAULT NOW() NOT NULL,
+    last_updated timestamp with time zone DEFAULT NOW() NOT NULL,
+    content jsonb DEFAULT '{}' NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT fk_diary FOREIGN KEY(id)  REFERENCES diary(id)
 );
