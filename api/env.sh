5c30206491e03bb54a0928c32758da381ed6b062
Hao Wu <wuhaoecho@gmail.com>
2023-05-12 10:20:15 +0800

    update

--- api/env.sh
+++ api/env.sh
@@ -1,2 +1,1 @@
-export BESTQA_FS_PORT=8087
-export BESTQA_PG_DSN=postgres://swuecho:VwbE1sYP9OgH@ep-fragrant-sound-219991-pooler.us-west-2.aws.neon.tech/note?sslmode=VerifyFull
+export DATABASE_URL=postgres://swuecho:VwbE1sYP9OgH@ep-fragrant-sound-219991-pooler.us-west-2.aws.neon.tech/note?sslmode=VerifyFull
