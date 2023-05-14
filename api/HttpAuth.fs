f6eb2347964d0e23ac9f6fba3b70ca30bd4eba3b
Hao Wu <wuhaoecho@gmail.com>
2023-05-15 00:07:23 +0800

    update

--- api/HttpAuth.fs
+++ api/HttpAuth.fs
@@ -1,8 +1,6 @@
 module HttpAuth
 
-open System.Text.Json
 open System.Net
-open Microsoft.AspNetCore.Http
 open Falco
 
 let forbidden =
@@ -14,19 +12,3 @@ let forbidden =
            message = message |}
 
 let AuthRequired h = Request.ifAuthenticated h forbidden
-
-
-module Authentication =
-    let isAuthenticated (context: HttpContext) =
-        context.User.Identity.IsAuthenticated = true
-
-    let Middleware (next: RequestDelegate) (context: HttpContext) =
-        // check path start with /api
-        if context.Request.Path.StartsWithSegments(PathString("/api")) then
-            if isAuthenticated context then
-                next.Invoke context
-            else
-                context.Response.StatusCode <- 401
-                context.Response.WriteAsync "Unauthorized"
-        else
-            next.Invoke context
