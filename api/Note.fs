3ad923e3edcbee2f044f8c0068847280350d728e
Hao Wu <wuhaoecho@gmail.com>
2023-05-15 00:42:12 +0800

    npgsql_connection (#18)
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- api/Note.fs
+++ api/Note.fs
@@ -4,6 +4,7 @@ open System.Net
 open Microsoft.AspNetCore.Http
 open Falco
 open System.Security.Claims
+open Npgsql
 
 let forbidden =
     let message = "Access to the resource is forbidden."
@@ -20,12 +21,21 @@ let AuthRequired h = Request.ifAuthenticated h forbidden
 /// from async objec to json response
 
 
+type HttpContext with
+
+    member this.getNpgsql() =
+        // implementation of your method
+        match this.Items.["NpgsqlConnection"] with
+        | :? NpgsqlConnection as connection -> connection
+        | _ -> failwith "can not get connection"
+
+
 // https://github.com/pimbrouwers/Falco/pull/41
 // do not cache result
 let noteAllPartSlow: HttpHandler =
     fun ctx ->
         let user_id = getUserId ctx.User
-        use conn = Database.Config.conn ()
+        let conn = ctx.getNpgsql ()
 
         Request.mapRoute
             (ignore)
@@ -39,7 +49,7 @@ let noteAllPart: HttpHandler =
     fun ctx ->
         // refresh note summary
         let user_id = int (ctx.User.FindFirst("user_id").Value)
-        use conn = Database.Config.conn ()
+        let conn = ctx.getNpgsql ()
 
         Request.mapRoute
             (ignore)
@@ -55,7 +65,7 @@ let getNoteById conn id user_id =
 
 let noteByIdPart: HttpHandler =
     fun ctx ->
-        use conn = Database.Config.conn ()
+        let conn = ctx.getNpgsql ()
 
         let getSurvey (route: RouteCollectionReader) =
             let user_id = getUserId ctx.User
@@ -69,7 +79,7 @@ let addNotePart: HttpHandler =
         Request.mapJson
             (fun (note: Diary) ->
                 let user_id = int (ctx.User.FindFirst("user_id").Value)
-                use conn = Database.Config.conn ()
+                let conn = ctx.getNpgsql ()
 
                 Diary.AddNote
                     conn
