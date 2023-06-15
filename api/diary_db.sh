74af1fe13bd7baf19713b4540b6f8cd634dbbf6e
Hao Wu <wuhaoecho@gmail.com>
2023-06-15 13:26:17 +0800

    update

--- api/diary_db.sh
+++ api/diary_db.sh
@@ -1,1 +1,2 @@
- psql postgresql://hwu:using555@stock.bestqa.net:5432/hwu 
+psql postgresql://hwu:using555@stock.bestqa.net:5432/hwu 
+DATABASE_URL=postgres://diary_log:ANe6cD17o6ZGNht@swuecho-chat-db.flycast:5432/diary_log?sslmode=disable
