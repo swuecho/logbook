9299a1b187736e5603db59388ee419ef50254b30
Hao Wu <wuhaoecho@gmail.com>
2024-08-30 19:35:08 +0800

    Refactor: Simplify editor extensions import and configuration in EditorWrapper.vue

--- web/src/views/EditorWrapper.vue
+++ web/src/views/EditorWrapper.vue
@@ -20,36 +20,7 @@ import { debounce } from 'lodash';
 import moment from 'moment';
 import { Icon } from '@iconify/vue2';
 import tableOfContents from '@iconify/icons-mdi/table-of-contents';
-
-import {
-  Doc,
-  Text,
-  Paragraph,
-  Heading,
-  Bold,
-  Underline,
-  Italic,
-  Strike,
-  Blockquote,
-  Code,
-  CodeBlock,
-  ListItem,
-  BulletList,
-  OrderedList,
-  TextAlign,
-  Indent,
-  LineHeight,
-  TrailingNode,
-  TodoItem,
-  TodoList,
-  History,
-  Fullscreen,
-  CodeView,
-  Link,
-  Iframe,
-  Image
-} from "element-tiptap";
-
+import { createExtensions } from '@/editorExt.js';
 import codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css'; // import base style
 import 'codemirror/mode/xml/xml.js'; // language
@@ -73,40 +44,7 @@ export default {
       icons: {
         tableOfContents,
       },
-      extensions: [
-        new Doc(),
-        new Text(),
-        new Paragraph(),
-        new Heading({ level: 3 }),
-        new Bold(),
-        new Underline({ bubble: true }),
-        new Italic(),
-        new Strike(),
-        new Blockquote({ bubble: true }),
-        new Code(),
-        new CodeBlock({ bubble: true }),
-        new TodoItem(),
-        new TodoList(),
-        new LineHeight(),
-        new ListItem(),
-        new BulletList(),
-        new OrderedList(),
-        new Indent(),
-        new TrailingNode(),
-        new TextAlign(),
-        new Link(),
-        new Image(),
-        new Iframe(),
-        new Fullscreen(),
-        new CodeView({
-          codemirror,
-          codemirrorOptions: {
-            styleActiveLine: true,
-            autoCloseTags: true,
-          },
-        }),
-        new History()
-      ]
+      extensions: createExtensions()
     };
   },
   created() {
