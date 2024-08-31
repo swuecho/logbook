fd4bac856840498b475e25a561297fefaf1acf97
Hao Wu <wuhaoecho@gmail.com>
2024-08-31 11:16:54 +0800

    Filter notes with length > 2 in noteAllPart handler

--- api/Note.fs
+++ api/Note.fs
@@ -58,7 +58,7 @@ let noteAllPart: HttpHandler =
                 Jieba.refreshSummary conn userId
 
                 Summary.GetSummaryByUserId conn userId
-                |> List.filter (fun x -> not (System.String.IsNullOrWhiteSpace x.Note))
+                |> List.filter (fun x ->  x.Note.Length > 2)
                 |> List.chunkBySize cols
                 |> Json.Response.ofJson)
             ctx
