81529abc14d4eb0f0a9e400f933ea99f6f931005
Hao Wu <wuhaoecho@gmail.com>
2024-09-01 11:03:56 +0800

    Refactor Login component to use Vue 3 Composition API and update form handling

--- web/src/components/Login.vue
+++ web/src/components/Login.vue
@@ -4,7 +4,7 @@
       <div slot="header" class="clearfix">Logbook</div>
       <el-main>
         <el-row>
-          <el-form ref="ruleForm" label-position="left" :model="form" status-icon :rules="rules" label-width="60px">
+          <el-form ref="ruleFormRef" label-position="left" :model="form" status-icon :rules="rules" label-width="60px">
             <el-form-item label="邮箱" prop="name">
               <el-input v-model="form.name" type="text" autocomplete="off" />
             </el-form-item>
@@ -13,7 +13,7 @@
               <el-input v-model="form.pwd" type="password" autocomplete="off" />
             </el-form-item>
           </el-form>
-          <el-button type="primary" size="medium" round @click="submit_form('ruleForm')">注册/登录</el-button>
+          <el-button type="primary" size="medium" round @click="submitForm">注册/登录</el-button>
         </el-row>
         <el-row class="alert-row">
           <div v-if="errors.length">
@@ -28,80 +28,66 @@
   </el-container>
 </template>
 
-<script>
-import axios from "axios";
+<script setup>
+import { ref, reactive } from 'vue';
+import axios from "@/axiosConfig.js";
+import router from '@/router';
 
-export default {
-  name: "Login",
-  data() {
-    return {
-      success: "",
-      form: {
-        name: "",
-        pwd: "",
-      },
-      errors: [],
-      rules: {
-        name: [
-          { required: true, message: "请输入邮箱", trigger: "blur" },
-          { type: "email", message: "不是正确的邮箱格式", trigger: "blur" },
-        ],
-        pwd: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 5, max: 100, message: "密码最少5个字符", trigger: "blur" },
-        ],
-      },
-    };
-  },
-  methods: {
-    submit_form(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          this.login();
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-    async login() {
-      // TODO: verify $router is avaliable
-      // clear error
-      this.errors = [];
+const ruleFormRef = ref(null);
+const success = ref('');
+const form = reactive({
+  name: '',
+  pwd: '',
+});
+const errors = ref([]);
+const rules = {
+  name: [
+    { required: true, message: "请输入邮箱", trigger: "blur" },
+    { type: "email", message: "不是正确的邮箱格式", trigger: "blur" },
+  ],
+  pwd: [
+    { required: true, message: "请输入密码", trigger: "blur" },
+    { min: 5, max: 100, message: "密码最少5个字符", trigger: "blur" },
+  ],
+};
 
-      let { name, pwd } = this.form;
-      // bestqa_fs
-      const options = {
-        method: "POST",
-        url: "/api/login",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          //ClientId: "bc442bb2b1d848fba5be2aa24312e711",
-          Username: name,
-          Password: pwd,
-        },
-      };
-      let app = this;
-      axios
-        .request(options)
-        .then(function (response) {
-          let data = response.data;
-          let jwt = data["accessToken"];
-          var seconds = new Date() / 1000;
-          let jwt_expired = data["expiresIn"] + seconds;
-          if (jwt) {
-            localStorage.setItem("JWT_TOKEN", jwt);
-            localStorage.setItem("JWT_EXPIRES_AT", jwt_expired)
-            app.$router.push({ path: "/" })
-          }
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+const submitForm = async () => {
+  if (!ruleFormRef.value) return;
+
+  const valid = await ruleFormRef.value.validate().catch(() => false);
+  if (valid) {
+    await login();
+  } else {
+    console.log("error submit!!");
+  }
+};
+
+const login = async () => {
+  errors.value = [];
+
+  const { name, pwd } = form;
+  const options = {
+    method: "POST",
+    url: "/api/login",
+    data: {
+      Username: name,
+      Password: pwd,
     },
-  },
+  };
+
+  try {
+    const response = await axios.request(options);
+    const { accessToken, expiresIn } = response.data;
+    if (accessToken) {
+      const expiresAt = new Date().getTime() + expiresIn * 1000;
+      localStorage.setItem("JWT_TOKEN", accessToken);
+      localStorage.setItem("JWT_EXPIRES_AT", expiresAt.toString());
+      router.push({ path: "/" });
+    }
+  } catch (error) {
+    console.error(error);
+    errors.value.push("Login failed. Please check your credentials and try again.");
+  }
 };
 </script>
 
