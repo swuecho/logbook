4e23167125d860e3ca3f6a398b5a0b4cca15d247
Hao Wu <wuhaoecho@gmail.com>
2024-12-28 13:50:27 +0800

    Remove idb dependency and related files
    Deleted the idb package and all related files from the node_modules directory. This removes the IndexedDB wrapper library that was previously installed as a dependency. The package.json file was also deleted as it only contained the idb dependency.

--- node_modules/idb/build/util.d.ts
+++
@@ -1,3 +0,0 @@
-export type Constructor = new (...args: any[]) => any;
-export type Func = (...args: any[]) => any;
-export declare const instanceOfAny: (object: any, constructors: Constructor[]) => boolean;
